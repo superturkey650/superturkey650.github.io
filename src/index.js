1c90378806aeb169d3776bac364ae411630466cf
Jared Mosley <35378004+superturkey650@users.noreply.github.com>
2020-09-22 21:19:49 +0000

    switch to commonjs library

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /** @jsx createElement */
 import {createElement} from "@bikeshaving/crank/cjs";
-import {renderer} from "@bikeshaving/crank/dom";
+import {renderer} from "@bikeshaving/crank/cjs/dom";
 
 renderer.render(<div id="hello">Hello world</div>, document.body);
